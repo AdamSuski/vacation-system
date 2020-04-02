5360e7d140ec9359194958903d5a3ad3e9d0b245
AdamSuski <adamus.suski@gmail.com>
2020-04-02 17:27:28 +0200

    spring boot init

---
+++ src/test/java/pl/suskia/system/vacation/VacationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.suskia.system.vacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
