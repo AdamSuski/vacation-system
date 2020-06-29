b0159187857f4af097e171e940ac218c3c27c11a
AdamSuski <adamus.suski@gmail.com>
2020-06-29 19:43:28 +0200

    update test

--- src/test/java/pl/suskia/system/vacation/VacationApplicationTests.java
+++ src/test/java/pl/suskia/system/vacation/VacationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VacationApplicationTests {
+class VacationApplicationTests extends VacationApplication {
 
     @Test
     void contextLoads() {
